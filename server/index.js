9937966120465bd3772d46f2df4948d768b2e296
Kenton <kentonlin1@gmail.com>
2016-11-23 19:30:43 -0500

    testing of various mws js libraries.. no success yet

--- server/index.js
+++ server/index.js
@@ -7,3 +7,7 @@ const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
+
+app.get('/', function(req, res) {
+  console.log('this is the artist');
+});

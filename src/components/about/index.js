cb397da2820aa55a697556d9c0f16fcdcfaacc86
Kenton <kentonlin1@gmail.com>
2016-11-26 15:56:04 -0500

    setup endpoints to add user, song and get user songs

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -18,19 +18,20 @@ export default class About extends Component {
   }
 
   componentDidMount(){
-    $.ajax({
-      type: 'GET',
-      url: 'https://api.spotify.com/v1/artists/20qISvAhX20dpIbOOzGK3q',
-      success: function(data){
-        console.log('this is the data', data);
-      }
-    });
+    // $.ajax({
+    //   type: 'GET',
+    //   url: 'https://api.spotify.com/v1/artists/20qISvAhX20dpIbOOzGK3q',
+    //   success: function(data){
+    //     console.log('this is the data', data);
+    //   }
+    // });
   }
 
   render() {
     const { className, ...props } = this.props;
     return (
       <div className={classnames('About', className)} {...props}>
+      <iframe src="https://embed.spotify.com/?uri=spotify:user:spotify:playlist:3rgsDhGHZxZ9sB9DQWQfuf" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>
         <h1>
           About
         </h1>

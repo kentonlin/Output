87bfa29744302c75503b8f24ebf5316257e979b5
Kenton <kentonlin1@gmail.com>
2016-11-26 01:24:49 -0500

    changes to test server

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -3,7 +3,6 @@ import classnames from 'classnames';
 import $ from 'jquery';
 
 import './style.css';
-var http = require('http');
 
 export default class About extends Component {
   // static propTypes = {}
@@ -19,39 +18,13 @@ export default class About extends Component {
   }
 
   componentDidMount(){
-    var MWS = require('mws-sdk-promises'),
-    client = new MWS.Client('AKIAJOVYKDEC5IYAQR3A', 'nySEuqkG2coIW53NER', 'A2LX4IL6EQVA63', {}),
-    MarketplaceId = "ATVPDKIKX0DER";
-
-
-    function getListOrders(client, args) {
-      var req = MWS.Orders.requests.ListOrders();
-      req.set(args);
-      return client.invoke(req);
-    }
-    var date = new Date();
-    getListOrders(client, {
-      MarketplaceId: MarketplaceId,
-      MaxResultsPerPage: 10,
-      CreatedAfter: new Date(2015, 1, 1),
-      CreatedBefore: new Date(2015, 1, 31)
-    })
-    .catch(function(error) {
-      console.error(error);
-    })
-    .then(function(RESULT){
-      console.log("--------");
-      console.log(JSON.stringify(RESULT));
-      console.log("--------");
+    $.ajax({
+      type: 'GET',
+      url: 'https://api.spotify.com/v1/artists/20qISvAhX20dpIbOOzGK3q',
+      success: function(data){
+        console.log('this is the data', data);
+      }
     });
-    // $.ajax({
-    //   type: 'GET',
-    //   url: 'https://api.spotify.com/v1/artists/20qISvAhX20dpIbOOzGK3q',
-    //   success: function(data){
-    //     console.log('this is the data', data);
-    //   }
-    // });
-
   }
 
   render() {
@@ -62,7 +35,6 @@ export default class About extends Component {
           About
         </h1>
         <div> {this.state.good} </div>
-        <div> {this.state.data} </div>
       </div>
     );
   }
